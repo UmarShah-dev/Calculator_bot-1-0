f38568830dbb7615aa0295a3bede44b492c2af9b
UmarShah-dev <syedumaralishah22@gmail.com>
2026-08-09 16:29:34 +0500

    Add files via upload

---
+++ calculator1.0.py
@@ -0,0 +1,15 @@
+print("===== SIMPLE CALCULATOR =====")
+# Take two numbers from the user
+number1 = float(input("Enter first number: "))
+number2 = float(input("Enter second number (not zero): "))
+# Perform calculations
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+division = number1 / number2
+# Display results
+print("\n===== RESULTS =====")
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
